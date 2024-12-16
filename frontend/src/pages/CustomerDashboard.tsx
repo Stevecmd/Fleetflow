1449a77dbf4f4f871bd2c9bd52aba72458ef1569
stevecmd <murimi101@gmail.com>
2024-12-16 19:20:54 +0300

    feat: Add CustomerDashboard component with welcome message

---
+++ frontend/src/pages/CustomerDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomerDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Customer Page</h1>
+        </div>
+    );
+};
+
+export default CustomerDashboard;
